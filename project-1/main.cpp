01bfde104b99399d8b84fbc5f73bbaebdf6047c6
Sachin <145639140+Derikklok@users.noreply.github.com>
2026-05-18 17:29:19 +0530

    Refactor main.cpp to define Status enum at the top and improve code organization

--- project-1/main.cpp
+++ project-1/main.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
-int main(){
+enum Status
+{
+    TODO,
+    PENDING,
+    COMPLETED
+};
+
+int main()
+{
     cout << "Hello World" << endl;
+
+    // Variables
+    int myNum = 8;
+    cout << myNum << endl;
+    double pi = 3.14;
+    cout << pi << endl;
+    char firstLetter = 'A';
+    cout << firstLetter << endl;
+    string name = "sachin";
+    cout << name << endl;
+
+    // enums
+    enum Status current = PENDING;
+    cout << current << endl;
+
+
     return 0;
 }
